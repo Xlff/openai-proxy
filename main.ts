ae9e49a5649752987bca9f9199524ccee654b88a
Xlff <xiemax09@gmail.com>
2023-04-12 19:46:22 +0800

    1

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
-const OPENAI_API_HOST = "api.openai.com";
+const OPENAI_API_HOST = "http://47.89.230.229:9005/";
 
 serve(async (request) => {
   const url = new URL(request.url);
 
-  if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
-  }
 
   url.host = OPENAI_API_HOST;
   return await fetch(url, request);
